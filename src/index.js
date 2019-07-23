3da3a16120ca08392b4e6ccbddb6da22ecf41850
Jansen <v.jansen.martin@gmail.com>
2019-07-23 14:58:59 -0700

    Add body-parser; Add CREATE route for users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 let express = require('express')
-
 let app = express()
-
 let path = require('path')
+let bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
     console.log(`${new Date().toString()} => ${req.originalUrl}`, req.body)
---
+++ src/routes/user.js
@@ -0,0 +1,27 @@
+let UserModel = require('../models/user.model')
+let express = require('express')
+let router = express.Router()
+
+//CREATE a new user
+//localhost:3000/user
+router.post('/user', (req, res) => {
+    if(!req.body) {
+      return res.status(400).send('Request body is missing')
+    }
+  
+    let model = new UserModel(req.body)
+    console.log(model);
+    model.save()
+      .then(doc => {
+        if(!doc || doc.length === 0) {
+          return res.status(500).send(doc)
+        }
+  
+        res.status(201).send(doc)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  })
+
+  module.exports = router

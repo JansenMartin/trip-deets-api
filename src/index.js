8607f419fb7373ddf373df63b89ae5441e8beb1b
Jansen <v.jansen.martin@gmail.com>
2019-07-23 14:31:56 -0700

    Add Error 500 handler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ let express = require('express')
 
 let app = express()
 
+let path = require('path')
+
 app.use((req, res, next) => {
     console.log(`${new Date().toString()} => ${req.originalUrl}`, req.body)
     next()
@@ -10,6 +12,12 @@ app.use((req, res, next) => {
 // Handler for 404 - Resource Not Found
 app.use((req, res, next) => {
     res.status(404).send('We think you are lost!')
+})
+
+// Handler for Error 500
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.sendFile(path.join(__dirname, '../public/500.html'))
 })
 
 app.use(express.static('public'))
